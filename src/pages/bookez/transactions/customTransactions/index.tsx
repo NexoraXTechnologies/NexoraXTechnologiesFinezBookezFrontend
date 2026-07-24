0f6ad677b4b05818ecae809b91cfb041e3b1d8bf
riyasahufinez-cloud <riyasahu.finez@gmail.com>
2026-07-24 12:15:37 +0530

    added custom transaction indexing

--- src/pages/bookez/transactions/customTransactions/index.tsx
+++ src/pages/bookez/transactions/customTransactions/index.tsx
@@ -1,56 +1,56 @@
-// import { useEffect, useMemo } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import {
-//     PackageSearch,
-//     ShieldCheck,
-// } from "lucide-react";
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    PackageSearch,
+    ShieldCheck,
+} from "lucide-react";
 
-// import TransactionDashboard from "../../../../components/mainPage/TransactionDashboard";
-// import { getAllTransactionModules } from "../../../../redux/slices/professionalSlice/transactionConfiguration/transactionModuleSlice";
+import TransactionDashboard from "../../../../components/mainPage/TransactionDashboard";
+import { getAllTransactionModules } from "../../../../redux/slices/professionalSlice/transactionConfiguration/transactionModuleSlice";
 
-// const CustomTransactionDashboard = () => {
-//     const dispatch = useDispatch<any>();
+const CustomTransactionDashboard = () => {
+    const dispatch = useDispatch<any>();
 
-//     const transactionModuleState = useSelector(
-//         (state: any) => state.transactionModule
-//     );
+    const transactionModuleState = useSelector(
+        (state: any) => state.transactionModule
+    );
 
    
-//     const transactionModules =
-//         transactionModuleState?.items ||
-//         transactionModuleState?.data?.items ||
-//         [];
+    const transactionModules =
+        transactionModuleState?.items ||
+        transactionModuleState?.data?.items ||
+        [];
 
-//     useEffect(() => {
-//         dispatch(
-//             getAllTransactionModules({
-//                 offset: 0,
-//                 limit: 50,
-//                 status: "active",
-//             })
-//         );
-//     }, [dispatch]);
+    useEffect(() => {
+        dispatch(
+            getAllTransactionModules({
+                offset: 0,
+                limit: 50,
+                status: "active",
+            })
+        );
+    }, [dispatch]);
 
-//     // console.log("Transaction modules:", transactionModules);
+    // console.log("Transaction modules:", transactionModules);
 
-//     const cards = useMemo(() => {
-//         return transactionModules.map((item: any) => ({
-//             title: item.moduleName || "Custom Transaction",
-//             description:item.description ||`Manage ${item.moduleName || "custom transaction"}.`,
-//             icon: <ShieldCheck size={22} />,
-//             path: `/professional/transaction/custom/${item.moduleCode}`,
-//             permissionKey: "Pass"
-//         }));
-//     }, [transactionModules]);
+    const cards = useMemo(() => {
+        return transactionModules.map((item: any) => ({
+            title: item.moduleName || "Custom Transaction",
+            description:item.description ||`Manage ${item.moduleName || "custom transaction"}.`,
+            icon: <ShieldCheck size={22} />,
+            path: `/professional/transaction/custom/${item.moduleCode}`,
+            permissionKey: "Pass"
+        }));
+    }, [transactionModules]);
 
-//     return (
-//         <TransactionDashboard
-//             title="Custom Transaction Workflow"
-//             description="Access and manage the custom transaction workflows configured for your business."
-//             icon={<PackageSearch size={24} />}
-//             cards={cards}
-//         />
-//     );
-// };
+    return (
+        <TransactionDashboard
+            title="Custom Transaction Workflow"
+            description="Access and manage the custom transaction workflows configured for your business."
+            icon={<PackageSearch size={24} />}
+            cards={cards}
+        />
+    );
+};
 
-// export default CustomTransactionDashboard;
+export default CustomTransactionDashboard;
