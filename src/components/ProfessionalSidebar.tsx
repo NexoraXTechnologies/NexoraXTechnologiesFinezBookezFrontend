9a7750dda25e5c069ba4b19e7321bfdaeeb5d853
riyasahufinez-cloud <riyasahu.finez@gmail.com>
2026-08-12 15:00:13 +0530

    productionadded in sidebar

--- src/components/ProfessionalSidebar.tsx
+++ src/components/ProfessionalSidebar.tsx
@@ -27,6 +27,7 @@ import {
 	MonitorCog,
 	Settings2,
 	Workflow,
+	Factory,
 } from "lucide-react";
 import ConfirmTooltip from "./common/ConfirmTooltip";
 // import { useDispatch } from "react-redux";
@@ -139,6 +140,14 @@ const ProfessionalSidebar = ({ onMenuItemsChange, onMobileClose }: any) => {
 							permissionKey: "openingBalance",
 							action: "view",
 						},
+						// {
+						// 	name: "Production Workflow",
+						// 	path: "/bookEz/transaction/production",
+						// 	icon: <Factory size={19} />,
+						// 	module: "bookez",
+						// 	permissionKey: "production",
+						// 	action: "view",
+						// },
 						{
 							name: "Sale Workflow",
 							path: "/bookEz/transaction/sale-workflow",
