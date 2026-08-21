b63829464ecb5acd03e527eab1399495cc8e4e10
NEXORA4 <saurabhlande.finez@gmail.com>
2026-08-21 19:03:05 +0530

    change

--- src/pages/setting/systemConfiguration/index.tsx
+++ src/pages/setting/systemConfiguration/index.tsx
@@ -806,7 +806,7 @@ const SystemConfiguration = () => {
                     ? "Configuration updated successfully"
                     : "Configuration saved successfully")
             );
-            // window.location.reload();
+            window.location.reload();
         } catch (err: any) {
             toast.error(
                 err?.message ||
