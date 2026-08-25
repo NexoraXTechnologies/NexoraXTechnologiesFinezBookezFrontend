4c12644be36c2f8ebfc9184a75361ebc04981a46
NEXORA4 <saurabhlande.finez@gmail.com>
2026-08-25 18:12:07 +0530

    add assembly production payload and dynamic field

--- src/components/voucher/SummaryCards.tsx
+++ src/components/voucher/SummaryCards.tsx
@@ -13,14 +13,14 @@ const SummaryCards = ({ items = [], isSummaryFooter = true }: SummaryCardsProps)
 
     return (
         isSummaryFooter ? <div className="mt-6 w-full rounded-md border border-border bg-card p-4">
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
                 {items.map((item) => (
                     <div
                         key={item.label}
-                        className="flex items-center justify-between rounded-lg bg-secondary px-5 py-4 text-sm font-semibold text-secondary-foreground"
+                        className="flex items-center justify-between border-b border-border  px-5 py-1 text-sm font-semibold text-secondary-foreground"
                     >
                         <span>{item.label}</span>
-                        <span>{item.value}</span>
+                        <span>{item.value || "--"}</span>
                     </div>
                 ))}
             </div>
